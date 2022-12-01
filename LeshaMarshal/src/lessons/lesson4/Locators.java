39458fc8b3382a3d4fb39e3b3be443e6cbc862b8
r.romadin <r.romadin@integriks.ru>
2022-12-01 10:36:20 +0300

    initial commit

--- LeshaMarshal/src/lessons/lesson4/Locators.java
+++ LeshaMarshal/src/lessons/lesson4/Locators.java
@@ -1,4 +1,24 @@
 package lesson4;
 
-public class Locators {
+import org.example.Main;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Locators  {
+
+
+/*
+    WebElement input  = driver = diver.findElement(By.XPath("//input[@aria-label='Найти']"))  - принимает в себя один аргумент
+    WebElement input  = driver = diver.findElements(By.XPath("//input[@aria-label='Найти']")) - несколько
+
+    WebElement input  = driver = diver.findElement(By.cssSelector())
+    WebElement input  = driver = diver.findElement(By.className())
+    WebElement input  = driver = diver.findElement(By.id())
+    WebElement input  = driver = diver.findElement(By.linkText())
+    WebElement input  = driver = diver.findElement(By.name())
+    WebElement input  = driver = diver.findElement(By.tagName())
+
+*/
+
 }
+
